f2ca307aa0025932e5662ffddaa5d656a540a80d
maximilianschmelzer <mschmelzer@protonmail.ch>
2020-08-11 23:14:35 +0200

    WIP I

---
+++ README.md
@@ -0,0 +1,35 @@
+# mangafreak to pdf
+
+Read as many chapters of your favourite manga as you desire. Locally. On your own e-reader.
+
+The script automatically downloads, unzips and converts mangafreak data to pdf files.
+
+## Requirements
+
+- Node (tested with v14.7.0)
+
+## Usage
+
+Install Dependencies
+<br>
+`npm i`
+
+Run script
+<br>
+`node manga`
+
+## Prompts
+
+### Name
+
+https://w11.mangafreak.net/Manga/ { name }
+
+Example:
+<br>
+https://w11.mangafreak.net/Manga/Detective_Conan -> Detective_Conan
+
+### Chapters
+
+The max. number of chapters to download. The script always starts at chapter 1.
+
+Example: 10 will result in Chapter 1 to 10.
---
+++ manga.js
@@ -0,0 +1,68 @@
+const request = require('request');
+const prompt = require('prompt');
+const fs = require('fs');
+const unzipper = require('unzipper');
+const PDFDocument = require('pdfkit');
+const path = require('path');
+
+prompt.start();
+
+prompt.get([
+  {
+    name: 'name',
+    required: true,
+  },
+  {
+    name: 'chapters',
+    required: true,
+  },
+], async (error, result) => {
+  for (let i = 1; i <= result.chapters; i++) {
+    const name = `${result.name}_${i}`;
+    const filePath = `./downloads/${name}.zip`;
+    const file = fs.createWriteStream(filePath);
+
+    const uri = `http://images.mangafreak.net:8080/downloads/${name}`;
+
+    await new Promise((resolveRequest) => {
+      request({ uri, gzip: true })
+        .pipe(file)
+        .on('finish', async () => {
+          const dir = `./downloads/${name}`;
+
+          fs.createReadStream(filePath)
+            .pipe(unzipper.Extract({ path: dir }))
+            .on('entry', (entry) => entry.autodrain())
+            .promise()
+            .then(() => {
+              fs.unlinkSync(filePath);
+
+              new Promise((resolvePdf) => fs.readdir(dir, (err, files) => {
+                const doc = new PDFDocument({ autoFirstPage: false });
+
+                doc.pipe(fs.createWriteStream(`${dir.split('_').join(' ')}.pdf`));
+
+                for (let ii = 1; ii <= files.length; ii++) {
+                  const imagePath = path.join(__dirname, `${dir}/${name.toLowerCase()}_${ii}.jpg`);
+
+                  const img = doc.openImage(imagePath);
+                  doc.addPage({ size: [img.width, img.height] }).image(imagePath, 0, 0);
+
+                  fs.unlinkSync(imagePath);
+                }
+
+                doc.end();
+
+                resolvePdf();
+              }))
+                .then(() => {
+                // UnhandledPromiseRejectionWarning: Error: EPERM: operation not permitted
+                // fs.unlinkSync(path.join(__dirname, 'downloads/' + name));
+                });
+            });
+
+          resolveRequest();
+        });
+    });
+  }
+});
